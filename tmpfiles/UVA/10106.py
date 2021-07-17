efc7cd86d60da52b9536d6d67c01d2a0ce14563e
Abdelrahman <abdokh950@gmail.com>
2021-07-17 18:45:28 +0200

    remove all problems

--- tmpfiles/UVA/10106.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        x=int (input())
-    except EOFError:
-        break
-    y=int(input())
-    print (str ( x*y))
-    
-        
--- tmpfiles/UVA/10106_1.py
+++
@@ -1,8 +0,0 @@
-while(1):
-    try:
-        x = int(input())
-        y = int(input())
-        z= x*y
-        print(z)
-    except:
-        break
